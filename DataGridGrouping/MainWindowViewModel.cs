891de8c5354e88cc99b90035f4bd091986d77bb8
Kazimir <avvlasov@outlook.com>
2021-11-05 14:35:29 +0300

    Создал архитектуру mvvm

---
+++ DataGridGrouping/MainWindowViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DataGridGrouping
+{
+    public class MainWindowViewModel
+    {
+        private ObservableCollection<Task> _MyTasks;
+        public ObservableCollection<Task> MyTasks
+        {
+            get { return _MyTasks; }
+            set => Set(ref _MyTasks, value);
+        }
+
+        public MainWindowViewModel()
+        {
+            GetTasks();
+        }
+
+        private void GetTasks()
+        {
+            MyTasks = new();
+            for (int i = 1; i <= 44; i++)
+            {
+                MyTasks.Add(new Task()
+                {
+                    ProjectName = "Project " + ((i % 5) + 1).ToString(),
+                    TaskName = "Task " + i.ToString(),
+                    DueDate = DateTime.Now.AddDays(i),
+                    Complete = (i % 2 == 0)
+                });
+            }
+        }
+
+        #region INotifyProperty
+        // Implement INotifyPropertyChanged interface.
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+        protected bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+        #endregion
+
+    }
+}
---
+++ DataGridGrouping/Task.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DataGridGrouping
+{
+    public class Task : INotifyPropertyChanged, IEditableObject
+    {
+        // Data for undoing canceled edits.
+        private Task temp_Task = null;
+        private bool m_Editing = false;
+
+        // Public properties.
+        private string _ProjectName = string.Empty;
+        public string ProjectName
+        {
+            get { return this._ProjectName; }
+            set => Set(ref _ProjectName, value);
+        }
+
+        private string _TaskName = string.Empty;
+        public string TaskName
+        {
+            get { return this._TaskName; }
+            set => Set(ref _TaskName, value);
+        }
+
+        private DateTime _DueDate = DateTime.Now;
+        public DateTime DueDate
+        {
+            get { return this._DueDate; }
+            set => Set(ref _DueDate, value);
+        }
+
+        private bool _Complete = false;
+        public bool Complete
+        {
+            get { return this._Complete; }
+            set => Set(ref _Complete, value);
+        }
+
+        // Implement IEditableObject interface.
+        public void BeginEdit()
+        {
+            if (m_Editing == false)
+            {
+                temp_Task = MemberwiseClone() as Task;
+                m_Editing = true;
+            }
+        }
+
+        public void CancelEdit()
+        {
+            if (m_Editing == true)
+            {
+                ProjectName = temp_Task.ProjectName;
+                TaskName = temp_Task.TaskName;
+                DueDate = temp_Task.DueDate;
+                Complete = temp_Task.Complete;
+                m_Editing = false;
+            }
+        }
+
+        public void EndEdit()
+        {
+            if (m_Editing == true)
+            {
+                temp_Task = null;
+                m_Editing = false;
+            }
+        }
+
+        #region INotifyProperty
+        // Implement INotifyPropertyChanged interface.
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+        protected bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+        #endregion
+
+    }
+}
